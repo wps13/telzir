914440fb7e0fbb835382bb0a9dce0f253a8f6212
Willane Paiva <willanepaiva@hotmail.com>
2020-01-13 09:50:29 -0300

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist "),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.j(s|sx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.(sc|c|sa)ss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss", ".css"],
+    alias: {
+      components: path.resolve(__dirname, "src/components/"),
+      pages: path.resolve(__dirname, "src/pages/")
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html"
+    })
+  ]
+};
