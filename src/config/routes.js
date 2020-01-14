091c2cf4de0ba61a8678168f1490e406881bb498
Willane Paiva <willanepaiva@hotmail.com>
2020-01-14 08:24:29 -0300

    update routes to include header

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,9 +4,11 @@ import { Switch, HashRouter, Route } from "react-router-dom";
 import Home from "pages/Home/Home";
 import Contact from "pages/Contact/Contact";
 import Plans from "pages/Plans/Plans";
+import Header from "components/Header/Header";
 
 const Routes = () => (
   <HashRouter>
+    <Header />
     <Switch>
       <Route path="/planos" component={Plans} />
       <Route path="/contato" component={Contact} />
