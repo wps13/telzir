ad05df79f40ac7f3157a8f6ea0bdb2979ad7ab09
Willane Paiva <willanepaiva@hotmail.com>
2020-01-16 10:15:00 -0300

    add prices data

---
+++ src/sistema/data/PRICES.js
@@ -0,0 +1,34 @@
+const PRICES = [
+  {
+    origin: 11,
+    destiny: 16,
+    price: 1.9
+  },
+  {
+    origin: 16,
+    destiny: 11,
+    price: 2.9
+  },
+  {
+    origin: 11,
+    destiny: 17,
+    price: 1.7
+  },
+  {
+    origin: 17,
+    destiny: 11,
+    price: 2.7
+  },
+  {
+    origin: 11,
+    destiny: 18,
+    price: 0.9
+  },
+  {
+    origin: 18,
+    destiny: 11,
+    price: 1.9
+  }
+];
+
+export { PRICES };
